ff5064541c0484dc52d0739c9cc1c3ada837dcc7
optimisme <optimisme@gmail.com>
2025-04-02 11:58:13 +0200

    .

--- 07 NodeJS - WebSockets/Exemple 0/copy_game.sh
+++ 07 NodeJS - WebSockets/Exemple 0/copy_game.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Copy game assets to 'server' and 'client'"
 cp -r flag_game* client_flutter/assets
 cp -r flag_game* server_nodejs/server
--- 07 NodeJS - WebSockets/Exemple 1/copy_game.sh
+++ 07 NodeJS - WebSockets/Exemple 1/copy_game.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Copy game assets to 'server' and 'client'"
 cp -r platform_game* client_flutter/assets
 cp -r platform_game* server_nodejs/server

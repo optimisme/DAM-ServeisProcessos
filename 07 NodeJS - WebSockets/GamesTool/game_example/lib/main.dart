1b235a5022d76a97f57d596dd6127be5039691eb
optimisme <optimisme@gmail.com>
2026-02-27 08:51:01 +0100

    Removed flame

--- 07 NodeJS - WebSockets/GamesTool/game_example/lib/main.dart
+++ 07 NodeJS - WebSockets/GamesTool/game_example/lib/main.dart
@@ -1,39 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:game_example/gameplay/game_screen.dart';
-import 'package:game_example/menu/level_menu_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'app_data.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const GamesToolExampleApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class GamesToolExampleApp extends StatelessWidget {
-  const GamesToolExampleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Games Tool',
-      theme: ThemeData.dark(),
-      home: LevelMenuScreen(
-        onLevelSelected:
-            (
-              BuildContext ctx,
-              String projectRoot,
-              String levelName,
-              int levelIndex,
-            ) {
-              Navigator.of(ctx).push(
-                MaterialPageRoute<void>(
-                  builder: (_) => GameScreen(
-                    projectRoot: projectRoot,
-                    levelName: levelName,
-                    levelIndexFallback: levelIndex,
-                  ),
-                ),
-              );
-            },
-      ),
-    );
-  }
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: const App(),
+    ),
+  );
 }
--- 07 NodeJS - WebSockets/GamesTool/game_example/lib/utils_gt/utils_gt.dart
+++
@@ -1,4 +0,0 @@
-export 'src/errors.dart';
-export 'src/models.dart'
-    hide JsonMap; // internal type alias, not part of the public API
-export 'src/repository.dart';
